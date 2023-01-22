958814cb3c73eb543b14ca3233c5ec82631c6c9f
noorintnt <noor@intnt.ai>
2023-01-23 02:03:14 +0800

    added documentation comments

--- tests/test_prediction.py
+++ tests/test_prediction.py
@@ -12,17 +12,21 @@ test_inputs = [("Hur mår du?", {'language':"Sweedish"}), ("Hey there how are yo
                ("как дела?", {'language':"Russian"}), ("எப்படி இருக்கிறீர்கள்?", {'language':"Tamil"}), ("كيف حالك", {'language':"Arabic"}), ("wie gehts?", {'language':"German"})]
 
 
+#test if healthcheck endpoint is working fne
 def test_read_main():
     response = client.get("/")
     assert response.status_code == 200
     assert response.json() == {'healthcheck': 'OK'}
 
-#using pytest parametrize to test multiple inputs
 
+
+#test if language endpoint is working fine , tests the iutput and status code
+#using pytest parametrize to test multiple inputs
 @pytest.mark.parametrize("sample, expected_output", test_inputs)
 def test_prediction(sample, expected_output):
     response = client.post(
             "language",
             json={"text":sample}
         )
+    assert response.status_code == 200
     assert response.json() == expected_output
